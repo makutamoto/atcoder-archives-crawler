fcb515392d8570a0fb4cf218d60a147e0ffe6af9
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-06-29 13:17:33 +0900

    + get_user_list

--- src/util/atcoder.rs
+++ src/util/atcoder.rs
@@ -1,12 +1,11 @@
 use std::error::Error;
-use std::str::FromStr;
 use std::collections::HashMap;
 
 use scraper::{ Html, Selector };
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-struct UserHistory {
+pub struct UserHistory {
     #[serde(rename = "IsRated")]
     is_rated: bool,
     #[serde(rename = "NewRating")]
@@ -18,12 +17,13 @@ struct UserHistory {
 }
 
 #[derive(Debug)]
-struct Contest {
+pub struct Contest {
     date: String,
     ranking: HashMap<String, u32>,
 }
 
 impl UserHistory {
+    #[cfg(test)]
     fn new(is_rated: bool, new_rating: u32, contest_screen_name: &str, end_time: &str) -> UserHistory {
         let contest_screen_name = String::from(contest_screen_name);
         let end_time = String::from(end_time);
@@ -32,12 +32,10 @@ impl UserHistory {
 }
 
 lazy_static! {
-    static ref SELECTOR_USER: Selector = Selector::parse("#main-container .table-responsive tbody tr").unwrap();
-    static ref SELECTOR_USER_NAME: Selector = Selector::parse(".username").unwrap();
-    static ref SELECTOR_USER_RATE: Selector = Selector::parse("td:nth-of-type(4)").unwrap();
+    static ref SELECTOR_USER_NAME: Selector = Selector::parse("#main-container .table-responsive tbody tr .username").unwrap();
 }
 
-fn get_user_history(name: &str) -> Result<Vec<UserHistory>, Box<dyn Error>> {
+pub fn get_user_history(name: &str) -> Result<Vec<UserHistory>, Box<dyn Error>> {
     let link = format!("https://atcoder.jp/users/{}/history/json", name);
     let res = reqwest::blocking::get(link.as_str())?;
     let json = res.text()?;
@@ -45,7 +43,7 @@ fn get_user_history(name: &str) -> Result<Vec<UserHistory>, Box<dyn Error>> {
     Ok(history)
 }
 
-fn assign_user(contests: &mut HashMap<String, Contest>, name: &str, history: &Vec<UserHistory>) {
+pub fn assign_user(contests: &mut HashMap<String, Contest>, name: &str, history: &Vec<UserHistory>) {
     let name = String::from(name);
     for contest_data in history {
         if contest_data.is_rated {
@@ -60,33 +58,14 @@ fn assign_user(contests: &mut HashMap<String, Contest>, name: &str, history: &Ve
     }
 }
 
-// pub fn get_rates(page: u32) -> Result<Vec<User>, Box<dyn Error>> {
-//     let mut result = Vec::new();
-//     let link = format!("https://atcoder.jp/ranking?page={}", page);
-//     let res = reqwest::blocking::get(link.as_str())?;
-//     let html = res.text()?;
-//     let document = Html::parse_document(html.as_str());
-//     let users = document.select(&SELECTOR_USER);
-//     for user in users {
-//         let name = user.select(&SELECTOR_USER_NAME).next();
-//         let rate = user.select(&SELECTOR_USER_RATE).next();
-//         if let (Some(name), Some(rate)) = (name, rate) {
-//             let name = name.text().next();
-//             let rate = rate.text().next();
-//             if let (Some(name), Some(rate)) = (name, rate) {
-//                 let name = String::from_str(name);
-//                 let rate = rate.parse();
-//                 if rate.is_err() {
-//                     eprintln!("Rate is not a number. User: {:?}", name);
-//                 }
-//                 if let (Ok(name), Ok(rate)) = (name, rate) {
-//                     result.push(User{ name: name, rate: rate });
-//                 }
-//             }
-//         }
-//     }
-//     Ok(result)
-// }
+pub fn get_user_list(page: u32) -> Result<Vec<String>, Box<dyn Error>> {
+    let link = format!("https://atcoder.jp/ranking/all?page={}", page);
+    let res = reqwest::blocking::get(link.as_str())?;
+    let html = res.text()?;
+    let document = Html::parse_document(html.as_str());
+    let user_names = document.select(&SELECTOR_USER_NAME);
+    Ok(user_names.map(|name| String::from(name.text().next().unwrap())).collect())
+}
 
 #[cfg(test)]
 mod tests {
@@ -119,5 +98,11 @@ mod tests {
         assert!(!contests.contains_key("abc002"));
         assert_eq!(contests["abc001"].date, "2016-09-04T22:50:00+09:00");
         assert_eq!(contests["abc001"].ranking, hashmap!{String::from("tourist") => 9999, String::from("Makutamoto") => 9});
+    }
+
+    #[test]
+    fn test_get_user_list() {
+        let list = get_user_list(1).unwrap();
+        assert_eq!(list.len(), 100);
     }
 }
